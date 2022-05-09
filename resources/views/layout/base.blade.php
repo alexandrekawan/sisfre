85b9f7ee1722db6265bd85f9b1bae45807fda4ac
alexandrekawan <alexandre.kawan@outlook.com>
2022-05-09 17:18:41 -0300

    Start login rework

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -12,6 +12,7 @@
     <link href="{{ asset('css/bootstrap.modif.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/estilo.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
     <noscript>
       <div class="container"> 
@@ -31,130 +32,8 @@
 </head>
 
 <body>
-<div class="container">
-    @if (Route::has('login'))
-
-	      <nav class="navbar navbar-default">
-	        <div class="container-fluid">
-	          <div class="navbar-header row col-md-3">
-	            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	              <span class="sr-only">Toggle navigation</span>
-	              <span class="icon-bar"></span>
-	              <span class="icon-bar"></span>
-	              <span class="icon-bar"></span>
-	            </button>
-
-                @if(Auth::check())
-                    <a class="navbar-brand" href="{{route('home')}}"><img src="{{asset('/img/sisfre.png')}}" class="img-responsive-mod" width="70%" height="70%"></a>
-                @else
-                    <a class="navbar-brand" href="{{route('login')}}"><img src="{{asset('/img/sisfre.png')}}" class="img-responsive-mod" width="70%" height="70%"></a>
-                @endif
-
-	          </div>
-    	          <div id="navbar" class="navbar-collapse collapse ">
-                    
-                    @if(Auth::check())
-    	            <ul class="nav navbar-nav">
-    	              @if( Auth::user()->acesso == 4)      	              
-                          <li class="dropdown">
-        	                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="false"> Cadastrar <span class="caret"></span></a>
-        	                <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{route('curso.create')}}"> Curso </a></li>
-        	                  <li><a href="{{route('disciplina.create')}}"> Disciplina </a></li>
-                            <li><a href="{{route('feriado.create')}}"> Feriado </a></li>
-                            <li><a href="{{route('sabado.create')}}"> Sábado Letivo </a></li>
-        	                  <li><a href="{{route('semestre.create')}}"> Semestre </a></li>
-                            <li><a href="{{route('usuario.create')}}"> Usuário </a></li>
-        	                </ul>
-        	              </li>
-
-                          <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="false"> Controle <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                              <li><a href="{{route('curso.show')}}"> Curso </a></li>
-                              <li><a href="{{route('disciplina.show')}}"> Disciplina </a></li>
-                              <li><a href="{{route('feriado.show')}}"> Feriado </a></li>
-                              <li><a href="{{route('sabado.show')}}"> Sábado Letivo </a></li>
-                              <li><a href="{{route('semestre.show')}}"> Semestre </a></li>
-                              <li><a href="{{route('usuario.show')}}"> Usuário </a></li>
-                            </ul>
-                          </li>
-                      @endif
-
-                      @if(Auth::user()->acesso == 1)
-                          
-                          <li class="dropdown">
-                              <a href="{{route('falta.create')}}"> Cadastrar Falta </a></li>
-                          </li>
-
-                          <li class="dropdown">
-                              <a href="{{route('falta.show.funcionario')}}"> Controle de Faltas </a></li>
-                          </li>
-
-                          <li class="dropdown">
-                              <a href="{{route('relatorio.index.funcionario')}}"> Relatórios </a></li>
-                          </li>
-                      @endif
-
-                      @if( Auth::user()->acesso == 3)
-                          <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="false"> Coordenador <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                              <li><a href="{{route('anteposicao.show.coordenador')}}"> Anteposições do Curso</a></li>
-                              <li><a href="{{route('falta.show.coordenador')}}"> Faltas do Curso </a></li>
-                              <li><a href="{{route('grafico.index')}}"> Gráficos </a></li>
-                              <li><a href="{{route('relatorio.index')}}"> Relatórios </a></li>
-                              <li><a href="{{route('reposicao.show.coordenador')}}"> Reposições do Curso </a></li>
-                              <li><a href="{{route('turma.show')}}"> Turmas do Curso </a></li>
-                            </ul>
-                          </li>
-
-                      @endif
-
-                      @if( Auth::user()->acesso == 3 or Auth::user()->acesso == 2)
-                          <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="false"> Professor <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                              <li><a href="{{route('horario.professor')}}"> Meu Horário  </a></li>
-                              <li><a href="{{route('falta.show.professor')}}"> Minhas Faltas </a></li>
-                              <li><a href="{{route('reposicao.show.professor')}}"> Minhas Reposições </a></li>
-                              <li><a href="{{route('anteposicao.show.professor')}}"> Minhas Anteposições </a></li>
-                            </ul>
-                          </li>
-                      @endif
-    	            </ul>
-                @endif
-
-                @if(Auth::check())
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="{{route('usuario.edit', Auth::user()->id) }}"><span class="glyphicon glyphicon-user"></span> {{Auth::user()->username}} </a></li>
-                        <li>
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                <span class="glyphicon glyphicon-log-in"></span> Sair
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                        </li>
-                    </ul>
-                @else
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-user"></span> Logar </a></li>
-                    </ul>
-                @endif
-	          </div><!--/.nav-collapse -->
-	        </div><!--/.container-fluid -->
-	      </nav>
-    @endif
-
-    <div class="row">
-        @yield('conteudo')
-    </div>
-
-    <footer class="welld navbar navbar-default footer container-fluid" style="color:#000;">
-            <div class="text-center"> &reg; copyright Sloth Software 2016-{{date('Y')}}. </div>
-    </footer>
+  
+    @yield('conteudo')
 
     <script src="{{ asset('js/app.js') }}"></script>
 </div>
--- resources/views/usuario/login.blade.php
+++ resources/views/usuario/login.blade.php
@@ -7,46 +7,20 @@
 @endsection
 
 @section('conteudo')
-        <div class="wrapper container-fluid">
-            <form action={{route('login')}} method="POST" class="form-signin">
-
-                <h2 class="form-login-heading"> <img src="./img/newLogo.png"> </h2>
-                <hr class="colorgraph"><br>
-                <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="control-label">E-mail: </label>
-
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="login" type="email" class="form-control" name="email" placeholder='Ex:admin@gmail.com' autofocus>
-                    </div>
-
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                    @endif
-                </div>
-
-                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="control-label">Senha: </label>
-
-                    <div class="input-group ">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="password" type="password" class="form-control" name="password">
-                    </div>
-
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                    @endif
-                </div>
-                {{csrf_field()}}
-                <button id="entrar" style="background: #00a859; color: white; width: 150px; margin: auto" type="submit" class="btn btn-lg btn-block "> Entrar </button>
-                <a class="btn btn-link" style="color: #373435" href="{{ route('password.request') }}">
-                    Esqueceu sua senha?
-                </a>
-            </form>
+    <div class="wrapper container-fluid d-flex justify-content-center align-items-center">
+    <form class="form-signin">
+        <div class="mb-3">
+            <label for="email" class="fw-bold form-label">E-Mail:</label>
+            <input type="email" class="form-control" name="email">
         </div>
+
+        <div class="mb-3">
+            <label for="password" class="fw-bold form-label">Senha:</label>
+            <input type="password" class="form-control" name="password">
+        </div>
+
+        <button type="submit" class="btn btn-success">Entrar</button>
+    </form>
+    </div>
     @include('layout.modal')
 @endsection
