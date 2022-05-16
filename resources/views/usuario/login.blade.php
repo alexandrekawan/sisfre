9ddf69c5ae164f5afddb885d6f637919901dad81
alexandrekawan <alexandre.kawan@outlook.com>
2022-05-16 17:05:25 -0300

    Finish Login Rework

--- resources/views/usuario/login.blade.php
+++ resources/views/usuario/login.blade.php
@@ -8,25 +8,25 @@
 
 @section('conteudo')
     <div class="wrapper container-fluid d-flex justify-content-center align-items-center">
-    <form class="form-signin" action={{route('login')}} method="POST">
-        {{csrf_field()}}
-        <div class="row align-items-center g-3">
-            <div class="form-group col-md-auto col-12 {{ $errors->has('email') ? ' has-error' : '' }}">
-                <label for="email" class="fw-bold form-label">E-Mail:</label>
-                <input type="email" class="form-control" name="email">
+        <form class="form-signin text-center" action={{route('login')}} method="POST">
+
+            {{ csrf_field() }}
+
+            <div class="form-login-heading">
+                <img src="./img/newLogo.png" class="img-fluid">
             </div>
 
-            <div class="form-group col-md-auto col-12 {{ $errors->has('password') ? ' has-error' : '' }}">
-                <label for="password" class="fw-bold form-label">Senha:</label>
-                <input type="password" class="form-control" name="password">
-            </div>
+            <input type="email" class="form-control" name="email" placeholder="E-Mail">
 
-            <div class="col-md-auto col-12">
-                <button type="submit" class="btn btn-success">Entrar</button>
-            </div>
-        </div>
-        
-    </form>
+            <input type="password" class="form-control" name="password" placeholder="Senha">
+
+            <button type="submit" class="form-control btn btn-success">Entrar</button>
+    
+
+            <a class="btn btn-link" href="{{ route('password.request') }}">Esqueceu sua senha?</a>
+                
+            
+        </form>
     </div>
     @include('layout.modal')
 @endsection
